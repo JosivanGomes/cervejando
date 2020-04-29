407dd231076adab7d2362a14e3ce8779441717b4
JOSIVAN GOMES DA SILVA JUNIOR <josivansilvajunior@gmail.com>
2020-04-29 09:00:44 -0300

    modal1 concluido

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,15 @@
-document //Apontar para documento 
-    .querySelector("#fat") //Escolher seletor pelo nome
+function liga_desliga(){
+    document
+        .querySelector("#modal")
+        .classList //criar o parametro lista
+        .toggle("hide") //tirar e colocar o valor da casse - escolhendo o nome
+}
+
+/*document //Apontar para documento 
+    .querySelector("#nv_ideia") //Escolher seletor pelo nome
     .addEventListener("click",function(){ //adicionar evento
         document
-            .querySelector("footer")
+            .querySelector("#modal")
             .classList //criar o parametro lista
             .toggle("hide") //tirar e colocar o valor da casse - escolhendo o nome
-    })
+    })*/
